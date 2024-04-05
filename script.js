4553d4ffc3657bc4a534c9697561e1d5f492d531
InDenz <denizc.317@outlook.com>
2024-04-05 23:33:09 +0200

    Done

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-const transactions = [
-    {
-        id: 1,
-        name: "salary",
-        amount: 5000,
-        date: new Date(),
-        type: "income"
-    },
-    {
-        id: 2,
-        name: "haircut",
-        amount: 20,
-        date: new Date(),
-        type: "expense"
-    },
-    {
-        id: 3,
-        name: "whatever",
-        amount: 100,
-        date: new Date(),
-        type: "expense"
-    },
-];
+const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -31,18 +9,34 @@ const formatter = new Intl.NumberFormat("en-US", {
 const list = document.getElementById("transactionList");
 const form = document.getElementById("transactionForm");
 const status = document.getElementById("status");
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
 
 form.addEventListener("submit", addTransaction);
+
+function updateTotal(){
+    const incomeTotal = transactions.filter(trx => trx.type === "income").reduce((total, trx) => total + trx.amount, 0);
+    const expneceTotal = transactions.filter(trx => trx.type === "expense").reduce((total, trx) => total + trx.amount, 0);
+
+    const balanceTotal = incomeTotal - expneceTotal;
+
+    balance.textContent = formatter.format(balanceTotal).substring(1);
+    income.textContent = formatter.format(incomeTotal);
+    expense.textContent = formatter.format(expneceTotal * -1);
+}
 
 function renderList() {
     list.innerHTML = "";
 
+    status.textContent = "";
     if(transactions.length === 0){
         status.textContent = "No transactions.";
         return;
     }
 
     transactions.forEach(({ id, name, amount, date, type })=>{
+        const sign = "income" === type ? 1 : -1;
         const li = document.createElement("li");
 
         li.innerHTML = `
@@ -51,7 +45,7 @@ function renderList() {
             <p>${new Date(date).toLocaleDateString()}</p>
             </div>
             <div class="amount ${type}">
-            <span>${formatter.format(amount)}</span>
+            <span>${formatter.format(amount*sign)}</span>
             </div>
             <div class="action">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
@@ -65,11 +59,15 @@ function renderList() {
 }
 
 renderList();
+saveTransactions();
+updateTotal();
 
 function deleteTransaction(id){
     const index = transactions.findIndex((trx) => trx.id === id);
     transactions.splice(index, 1);
     
+    updateTotal();
+    saveTransactions();
     renderList();
 }
 
@@ -84,9 +82,19 @@ function addTransaction(e){
         name: formData.get("name"),
         amount: parseFloat(formData.get("amount")),
         date: new Date(formData.get("date")),
+        type: "on" === formData.get("type") ? "income" : "expense",
     });
 
     this.reset();
 
+    updateTotal();
+    saveTransactions();
     renderList();
+}
+
+function saveTransactions() {
+
+    transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
 }
