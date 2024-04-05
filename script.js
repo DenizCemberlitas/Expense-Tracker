5ba35fef69a384c323a1e627d6ee8b6e84bf0249
Deniz Cemberlitas <denizc.317@outlook.com>
2024-04-05 17:57:26 +0200

    13:28

--- script.js
+++ script.js
@@ -4,28 +4,37 @@ const transactions = [
         name: "salary",
         amount: 5000,
         date: new Date(),
-        type: "income",
+        type: "income"
     },
     {
         id: 2,
         name: "haircut",
         amount: 20,
         date: new Date(),
-        type: "expense",
+        type: "expense"
     },
     {
         id: 3,
-        name: "concert ticket",
-        amount: 350,
+        name: "whatever",
+        amount: 100,
         date: new Date(),
-        type: "expense",
+        type: "expense"
     },
 ];
 
+const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    signDisplay: "always",
+});
+
 const list = document.getElementById("transactionList");
+const form = document.getElementById("transactionForm");
 const status = document.getElementById("status");
 
-function renderList(){
+form.addEventListener("submit", addTransaction);
+
+function renderList() {
     list.innerHTML = "";
 
     if(transactions.length === 0){
@@ -33,14 +42,22 @@ function renderList(){
         return;
     }
 
-    transactions.forEach((name)=>{
-        const li = document.createElement('li');
+    transactions.forEach(({ id, name, amount, date, type })=>{
+        const li = document.createElement("li");
 
         li.innerHTML = `
-        <div class="name">
+            <div class="name">
             <h4>${name}</h4>
             <p>${new Date(date).toLocaleDateString()}</p>
-        </div>
+            </div>
+            <div class="amount ${type}">
+            <span>${formatter.format(amount)}</span>
+            </div>
+            <div class="action">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            </div>
         `;
 
         list.appendChild(li);
@@ -48,3 +65,28 @@ function renderList(){
 }
 
 renderList();
+
+function deleteTransaction(id){
+    const index = transactions.findIndex((trx) => trx.id === id);
+    transactions.splice(index, 1);
+    
+    renderList();
+}
+
+function addTransaction(e){
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    
+
+    transactions.push({
+        id: transactions.length + 1,
+        name: formData.get("name"),
+        amount: parseFloat(formData.get("amount")),
+        date: new Date(formData.get("date")),
+    });
+
+    this.reset();
+
+    renderList();
+}
